49278224edfea5fb4be1e7afeb0a2d74da0b559d
George Tebbetts <george.tebbetts@gmail.com>
2011-11-16 20:11:08 +0000

    kinda does some stuff

--- webserver.py
+++ webserver.py
@@ -1,17 +1,41 @@
 #!/usr/bin/env python2.6
 
-from twisted.web import server, resource
+from twisted.web.server import Site
+from twisted.web.resource import Resource
 from twisted.internet import reactor
 
-class Event(resource.Resource):
+class Event(Resource):
+
+    def __init__(self, eventType):
+        Resource.__init__(self)
+        self.eventType = eventType
+
     def render_GET(self, request):
         request.setHeader("context-type", "text/plain")
-        return ""
+        return self.eventType + '+'
 
-root = Resource()
-root.putChild('e', Event())
+    def render_POST(self, request):
+        return self.eventType + '-'
 
-factory = server.Site(root)
+class EventScheduler(Resource):
+    def getChild(self, name, request):
+        if name == '':
+            return self
+
+        return Event(name)
+
+    def render_GET(self, request):
+        return 'et phone home'
+
+class RingLeader(Resource):
+    def render_GET(self, request):
+        return "welcome home"
+
+root = RingLeader()
+root.putChild('e', EventScheduler())
+
+factory = Site(root)
 
 reactor.listenTCP(8080, factory)
 reactor.run()
+
